b42df57de19b52481095482ae28ff13b00a2c1b5
sspangler <sspangler@ltu.edu>
2015-10-29 23:58:37 -0400

    Parallax and backgrounds

---
+++ Pathfinder/Assets/Scripts/Parallax.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public bool close, mid, far;
+	float speed;
+
+	public PlayerController player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+		if (close)
+			speed = 200F;
+		else if (mid) 
+			speed = 100;
+		else 
+			speed = 50f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3 (player.currentSpeed * speed / ( 25 * transform.position.z) * Time.deltaTime, 0,0);
+
+	}
+
+	void FixedUpdate () {
+
+
+	}
+}
---
+++ Pathfinder/Assets/Scripts/ParallaxScott.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScott : MonoBehaviour {
+	
+	public float para_percent = 1f;
+	float origin;
+	GameObject theplayer;
+	// Use this for initialization
+	void Start () {
+		theplayer = GameObject.FindGameObjectWithTag ("Player");
+		origin = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float difference;
+		difference = theplayer.transform.position.x - origin;
+		if((difference>0)||(difference<0))
+		{
+			transform.position = new Vector3(theplayer.transform.position.x
+			                                 - difference * para_percent,
+			                                 transform.position.y,
+			                                 transform.position.z);
+		}
+		else
+			transform.position = new Vector3(origin, transform.position.y, transform.position.z);
+	}
+}
