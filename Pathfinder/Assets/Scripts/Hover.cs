116b2bc84aabc646817203b17b583cef87544e69
sspangler <sspangler@ltu.edu>
2015-11-04 01:33:41 -0500

    Animations

---
+++ Pathfinder/Assets/Scripts/Hover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+
+	public Vector3 upPos, downPos;
+
+	// Use this for initialization
+	void Start () {
+	
+		upPos = transform.position + Vector3.up;
+		downPos = transform.position + Vector3.down;
+
+	}
+
+	public bool moveUp;
+	// Update is called once per frame
+	void Update () {
+		if (moveUp) {
+			transform.position = Vector3.MoveTowards(transform.position, upPos, 1 * Time.deltaTime);
+		} else {
+			transform.position = Vector3.MoveTowards(transform.position, downPos, 1 * Time.deltaTime);
+		}
+
+		if (transform.position == upPos)
+			moveUp = false;
+		else if (transform.position == downPos)
+			moveUp = true;
+	}
+}
