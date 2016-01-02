0d5531a11d389ab2b98068a6474baf769e9c9124
unknown <cvadella@MP05VK4M.campus.ltu.edu>
2016-01-02 14:30:26 -0500

    respawn after fall off the world

---
+++ Pathfinder/Assets/Scripts/Death.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+    GameObject Player;
+    GameObject RespawnPt;
+    int minY = -15;
+
+	// Use this for initialization
+	void Awake () {
+	//use the ridiculous death box in the future assumably, but for now, this'll do
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Player.transform.position.y < minY)
+        {
+            RespawnPt = GameObject.FindGameObjectWithTag("Respawn");
+            transform.position = RespawnPt.transform.position;
+        }
+	}
+}
