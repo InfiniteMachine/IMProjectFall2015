7af7084021a5a1a2fce01a5d777ee80da7119a81
sspangler <sspangler@ltu.edu>
2016-01-03 01:23:38 -0500

    Attack Plant

---
+++ Pathfinder/Assets/AttackPlant.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackPlant : MonoBehaviour {
+	public Transform player;
+
+	public float delay;
+	bool inRange;
+
+	public float distance;
+	float timer;
+
+	GameObject plant;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		timer = delay;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector2.Distance (transform.position, player.position);
+		if (distance < 10) {
+			inRange = true;
+		} else {
+			inRange = false;
+		}
+
+		if (timer > 0 && inRange)
+			timer -= Time.deltaTime;
+
+		if (timer < 0) {
+			//animation
+			if (distance < 2) {
+				player.GetComponent<HealthEffect> ().ReduceHealth ();
+				timer = delay;
+			}
+		}
+	}
+}
