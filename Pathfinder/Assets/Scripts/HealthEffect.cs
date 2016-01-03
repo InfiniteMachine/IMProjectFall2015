1938c7b9eb75d3acfdd89fcf822c4270328404d8
sspangler <sspangler@ltu.edu>
2016-01-02 19:47:27 -0500

    Health UI, and refining of lights/adding of a yellow light

--- Pathfinder/Assets/Scripts/HealthEffect.cs
+++ Pathfinder/Assets/Scripts/HealthEffect.cs
@@ -4,23 +4,48 @@ using System.Collections;
 
 public class HealthEffect : MonoBehaviour {
 
-	public Image healthUI;
-	public Material nearDeath;
-	public Material fullHealth;
-	public float duration = 2.0f;
-	public float health;
-	public float maxHealth;
+	public Image oneHealth;
+	public Image twoHealth;
+	public float duration = 1.0f;
+	int health;
+	Color fullAlpha = new Color(0,0,0,245f);
 
-	public Vector3 fullHealthScale;
-	public Vector3 nearDeathScale;
+	float t;
+
+	int prevHealth;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//healthUI.material.Lerp (nearDeath, fullHealth, health / maxHealth);
+		//health decreasing
+		if (health == 1 && prevHealth == 2 && t < 1) {
+			oneHealth.color = Color.Lerp (Color.clear, Color.white, t);
+			twoHealth.color = Color.Lerp (Color.white, Color.black, 1);
+		} else if (health == 2 && prevHealth == 3 && t < 1) {
+			twoHealth.color = Color.Lerp (Color.clear, Color.white, t);
+		}
+
+		if (health == 2 && prevHealth == 1 && t < 1) {
+			oneHealth.color = Color.Lerp (Color.white, Color.clear, t);
+			twoHealth.color = Color.Lerp (Color.black, Color.white, t);
+		} else if (health == 3 && prevHealth == 2 && t < 1) {
+			twoHealth.color = Color.Lerp (Color.white, Color.clear, t);
+		}
+			
+
+		if (t < 1) {
+			t += Time.deltaTime / duration;
+		}
+	}
+
+
+	public void UpdateHealth (int curHealth) {
+		prevHealth = health;
+		health = curHealth;
+		t = 0;
 	}
 }
