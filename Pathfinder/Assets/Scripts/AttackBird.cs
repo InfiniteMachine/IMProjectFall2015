ffde3c1ca5aff4b4b8967355804d58d12344cb04
sspangler <sspangler@ltu.edu>
2016-01-03 00:03:27 -0500

    particle effects for lights

--- Pathfinder/Assets/Scripts/AttackBird.cs
+++ Pathfinder/Assets/Scripts/AttackBird.cs
@@ -3,16 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class AttackBird : MonoBehaviour {
-	public GameObject leftNest, rightNest;
+	public Transform player;
+	public GameObject leftNest, rightNest, bird;
 	public float speed;
 	public float delay;
 	float timer;
 	bool isLeft;
 	bool inRange;
 
+	public float distance;
+
 	Vector3 leftNestPos, rightNestPos;
 	// Use this for initialization
 	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
 		leftNestPos = leftNest.transform.localPosition;
 		leftNestPos.y += 1;
 		rightNestPos = rightNest.transform.localPosition;
@@ -21,34 +25,30 @@ public class AttackBird : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		distance = Vector2.Distance (transform.position, player.position);
+		if (distance < 10) {
+			inRange = true;
+		} else {
+			inRange = false;
+		}
+
 		if (delay > 0)
 			delay -= Time.deltaTime;
 
 		if (inRange && delay <= 0) {
 			if (isLeft) {
-				transform.localPosition = Vector3.MoveTowards(transform.localPosition, rightNestPos, speed * Time.deltaTime);
-				if (transform.localPosition == rightNestPos) {
+				bird.transform.localPosition = Vector3.MoveTowards(bird.transform.localPosition, rightNestPos, speed * Time.deltaTime);
+				if (bird.transform.localPosition == rightNestPos) {
 					isLeft = false;
 					timer = delay;
 				}
-			}
-			else {
-				transform.localPosition = Vector3.MoveTowards(transform.localPosition, leftNestPos, speed * Time.deltaTime);
-				if (transform.localPosition == leftNestPos) {
+			} else {
+				bird.transform.localPosition = Vector3.MoveTowards(bird.transform.localPosition, leftNestPos, speed * Time.deltaTime);
+				if (bird.transform.localPosition == leftNestPos) {
 					isLeft = true;
 					timer = delay;
 				}
 			}
 		}
-	}
-
-	void OnTriggerEnter2D (Collider2D col) {
-		if (col.tag == "Player")
-			inRange = true;
-	}
-
-	void OnTriggerExit2D (Collider2D col) {
-		if (col.tag == "Player")
-			inRange = false;
 	}
 }
